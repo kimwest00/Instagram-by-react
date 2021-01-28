3b2d1087b12f4776638e4b131180f204f1c64d35
minseo <kim-west00@naver.com>
2021-01-28 17:06:16 +0900

    그림 추가

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {GoHeart} from 'react-icons/go';
+import boo_image from './boo.jpg';
 class Counter extends Component{
     constructor(props){
         super(props);
@@ -11,6 +12,12 @@ class Counter extends Component{
         const {number} = this.state;
         return(
             <div>
+                <img 
+                src={ boo_image }
+                width='540'
+                height='360'
+                 
+                />
                 <h1>{number}</h1>
                 <button
                 onClick={()=>{
@@ -20,6 +27,7 @@ class Counter extends Component{
                 <GoHeart/>
                     좋아요를 눌러
                 </button>
+
             </div>
         );
     }
